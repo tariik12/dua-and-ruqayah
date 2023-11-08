b793d36443f00487f18d1c210d8c297e7cfa4e9b
tarikul-islam <tariikul12@gmail.com>
2023-11-08 07:00:10 +0600

    project setup

---
+++ src/app/(root)/page.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import TopNav from './components/TopNav';
+
+const Home = () => {
+  return (
+    <main>
+      <TopNav></TopNav>
+    </main>
+  );
+};
+
+export default Home;
